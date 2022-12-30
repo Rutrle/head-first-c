935c8d2329a5be174cb7c7dc82c370a1bc36cfeb
Jakub Rutrle <jakubrutrle@gmail.com>
2022-12-30 13:04:42 +0100

    basic loops

---
+++ getting_started/cards_cycle.c
@@ -0,0 +1,46 @@
+/*
+ * Program to evaluate face values.
+ * Released under the Vegas Public License.
+ * (c)2014 The College Blackjack Team.
+ */
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+    char card_name[3];
+    int count = 0;
+    do {
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
+    switch (card_name[0]){
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            printf("count is %i", count);
+            return 0;
+        default:
+            val = atoi(card_name);
+            if (val<1 || val >10){
+                puts("wrong input");
+                continue;
+            }
+                
+    }
+
+    if (val >= 3 && val <=6)
+        count++;
+    else if (val >= 10)
+        count--;
+
+    printf("Current count: %i\n", count);
+    } while (1);
+    return 0;
+}
